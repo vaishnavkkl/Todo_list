e4784efe28fb0435a238bbe6e6abd0f29feb0c5d
vaishnavkkl <vaishnavkkl25@gmail.com>
2020-10-01 23:34:04 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items =["buy"];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine','ejs');
+
+app.get("/", function(req, res){
+
+	var today = new Date();
+	
+	var options = {
+		weekday:'long', day:'numeric', month:'long'
+	};
+
+	var day = today.toLocaleDateString("en-US", options);
+
+	res.render("list", {kindofDay: day, listItem: items});
+	
+});
+
+app.post("/", function(req, res){
+
+	 var item = req.body.item;
+	 items.push(item);
+	res.redirect("/");
+});
+
+app.listen(3000, function(){
+	console.log("server running");
+});
